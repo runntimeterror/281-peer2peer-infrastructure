62a7c82d2c0f3582aefe377ee4f8fefb55a83d27
Gabriel Chen <gabriel.luke.chen@gmail.com>
2021-11-17 09:43:11 -0800

    reimport new redis cluster?

--- modules/socket_service/main.tf
+++ modules/socket_service/main.tf
@@ -1,11 +1,21 @@
 resource "aws_elasticache_cluster" "moochat_redis_cluster" {
-  cluster_id           = "${terraform.workspace}-moochat-redis"
-  engine               = "redis"
-  node_type            = "cache.t2.micro"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
-  port                 = 6379
+  cluster_id           = "moo-chat-redis"
+  availability_zone        = "us-east-1a"
+  az_mode                  = "single-az"
+  port              = 6379
+  engine                   = "redis"
+  node_type                = "cache.t2.micro"
+  num_cache_nodes          = 1
+  parameter_group_name     = "default.redis6.x"
+  security_group_ids       = [
+    "sg-0b03f389b81b18623",
+    "sg-0d3fee5cb05cef4b4",
+  ]
+  snapshot_retention_limit = 0
+  snapshot_window          = "05:00-06:00"
+  subnet_group_name        = "default"
+  tags                     = {}
+  tags_all                 = {}
 }
 
 resource "aws_ecr_repository" "moochat_ecr_repo" {
@@ -26,5 +36,5 @@ output "moochat_ecr_repo_url" {
 }
 
 output "moochat_redis_cluster_address" {
-  value = resource.aws_elasticache_cluster.moochat_redis_cluster.cluster_address
+  value = resource.aws_elasticache_cluster.moochat_redis_cluster.arn
 }
